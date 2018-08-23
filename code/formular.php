b1abf5da0bfca9c760e247ff2acf1dc19c94bfbf
minikara <diane.leblanc.albarel@gmail.com>
2018-08-23 14:07:19 +0100

    calcul unit of alccol + redirection vers la page avec unit des recommendations

--- code/formular.php
+++ code/formular.php
@@ -24,25 +24,25 @@
 			<p>
 				Are you ?
 				<br />
-				<input type="radio" name="gender" id="women" /> <label for="women">A women</label><br />
-				<input type="radio" name="gender" id="men" /> <label for="men">A men</label><br />
+				<input type="radio" name="gender" value="women"id="women" required/> <label for="women">A women</label><br />
+				<input type="radio" name="gender" value="men" id="men" /> <label for="men">A men</label><br />
 			</p>
 			<p>
 				In a typically week, how many times do you drink ? <br />
 				<ul>
-					<li> <label for="pint"> A pint : </label> <input type="number" name="pint" value="0" class="quantity"/><label for="pint"> times </label> </li>
+					<li> <label for="pint"> A pint : </label> <input type="number" name="pint" value="0" min="0" max="50" class="quantity"/><label for="pint"> times </label> </li>
 					
-					<li> <label for="wine"> A glass of wine : </label> <input type="number" name="wine" value="0" class="quantity"/><label for="wine"> times </label> </li>
+					<li> <label for="wine"> A glass of wine : </label> <input type="number" name="wine" value="0" min="0" max="50" class="quantity"/><label for="wine"> times </label> </li>
 					
-					<li> <label for="Galcolpop"> A glass of alcolpop : </label> <input type="number" name="Galcolpop" value="0" class="quantity"/> <label for="Galcolpop"> times </label> </li>
+					<li> <label for="Galcolpop"> A glass of alcolpop : </label> <input type="number" name="Galcolpop" value="0" min="0" max="50" class="quantity"/> <label for="Galcolpop"> times </label> </li>
 					
-					<li> <label for="Balcolpop"> A bottle of alcolpop : </label> <input type="number" name="Balcolpop" value="0" class="quantity"/> <label for="Balcolpop"> times </label> </li>
+					<li> <label for="Balcolpop"> A bottle of alcolpop : </label> <input type="number" name="Balcolpop" value="0" min="0" max="50" class="quantity"/> <label for="Balcolpop"> times </label> </li>
 
-					<li> <label for="sparWine"> A glass of sparkling wine : </label> <input type="number" name="sparWine" value="0" class="quantity"/> <label for="sparWine"> times </label> </li>
+					<li> <label for="sparWine"> A glass of sparkling wine : </label> <input type="number" name="sparWine" value="0" min="0" max="50" class="quantity"/> <label for="sparWine"> times </label> </li>
 
-					<li> <label for="spirit"> A pub measure of spirits : </label> <input type="number" name="spirit" value="0" class="quantity"/> <label for="spirit"> times </label> </li>
+					<li> <label for="spirit"> A pub measure of spirits : </label> <input type="number" name="spirit" value="0" min="0" max="50" class="quantity"/> <label for="spirit"> times </label> </li>
 
-					<li> <label for="shot"> A shot : </label> <input type="number" name="shot" value="0" class="quantity"/> <label for="shot"> times </label> </li>
+					<li> <label for="shot"> A shot : </label> <input type="number" name="shot" value="0" class="quantity" min="0" max="50" /> <label for="shot"> times </label> </li>
 				</ul>
 
 			</p>
@@ -50,34 +50,34 @@
 			<p>
 				If you drink at least one time per week, how many days per week do you drink alcohol? (if you drink less than once a week just let 0) ? <br />
 					
-					<label for="times"><input type="number" name="times" value="0" class="quantity"/> times per week </label> <br />
+					<label for="times"><input type="number" name="times" value="0" class="quantity" min="0" max="7" /> times per week </label> <br />
 
 			</p>
 			
 			<p>
 				On the same occasion, what is the maximum of drink that you can take? <br />
-				<label for="max"><input type="number" name="max" value="0" class="quantity"/> drinks </label> <br />
+				<label for="max"><input type="number" name="max" value="0" class="quantity" min="0" max="30"/> drinks </label> <br />
 			</p>
 
 			<p>
 				Have you already been arrest because of your consomation of alcohol? <br />
 
-				<input type="radio" name="arrest" id="yes" /> <label for="yes">yes</label><br />
-				<input type="radio" name="arrest" id="no" /> <label for="no">no</label><br />
+				<input type="radio" name="arrest" value="yes" id="yes" required/> <label for="yes">yes</label><br />
+				<input type="radio" name="arrest" value="no" id="no" /> <label for="no">no</label><br />
 
 			</p>
 
 			<p>
 				Have you already had several blackouts? <br />
-				<input type="radio" name="blackout" id="yes" /> <label for="yes">yes</label><br />
-				<input type="radio" name="blackout" id="no" /> <label for="no">no</label><br />
+				<input type="radio" name="blackout" value="yes" id="yes" required /> <label for="yes">yes</label><br />
+				<input type="radio" name="blackout" value="no" id="no" /> <label for="no">no</label><br />
 			</p>
 
 			<p>
 				Has a friend, or any health professional ever been concerned about your drinking and advise you to decrease it? <br />
 
-				<input type="radio" name="advise" id="yes" /> <label for="yes">yes</label><br />
-				<input type="radio" name="advise" id="no" /> <label for="no">no</label><br />
+				<input type="radio" name="advise" value="yes" id="yes" required /> <label for="yes">yes</label><br />
+				<input type="radio" name="advise" value="no" id="no" /> <label for="no">no</label><br />
 			</p>
 
 			<p>
@@ -86,7 +86,4 @@
 		</form>
 
 	</body>
-	<p>
-		happy new year!!!!
-	</p>
 </html>
---
+++ code/result.php
@@ -0,0 +1,64 @@
+<!-- Function -->
+
+<?php
+
+$womenRecommendation = 11;
+$menRecommendation = 17;
+
+function CalculUnit ($pint, $wine, $Galcolpop, $Balcolpop, $sparkling_wine, $spirit, $shot) 
+{
+	$totalUnit = $pint * 2 + $shot + $spirit + $sparkling_wine + $wine + $Balcolpop + 0.5 * $Galcolpop ;
+
+	return $totalUnit; 
+}
+
+function Recommendation ($gender, $women, $men)
+{
+	if ($gender == "women")
+
+	return $women;
+
+elseif ($gender == "men")
+
+	return $men;
+
+else
+	return 'No gender';
+
+}
+
+$recommendation = Recommendation ($_POST['gender'], $womenRecommendation, $menRecommendation);
+
+$quantityConssum = CalculUnit ($_POST['pint'], $_POST['wine'], $_POST['Galcolpop'], $_POST['Balcolpop'], $_POST['sparWine'], $_POST['spirit'], $_POST['shot']);
+
+
+?>
+
+
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+    	<!-- Head of the page -->
+
+        <meta charset="utf-8" />
+
+        <title>Titre</title>
+
+    </head>
+
+    <body>
+
+    <?php
+
+    	echo '<p> Vous consommez ' . $quantityConssum . ' unite d\'alcool par semaine </p>';
+    	echo '<p> Les recommendations pour vous sont de ' . $recommendation . ' unite par semaine'; 
+
+    ?>
+    	
+    </body>
+
+
+</html>
